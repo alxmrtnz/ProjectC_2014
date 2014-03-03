4ea86687aeae8218a6881b6a12e236c00ac82345
Alex Martinez <amart1892@gmail.com>
2014-03-03 13:18:33 -0500

    adding primary menu to wordpress

--- wp-content/themes/Project_C_2014/header.php
+++ wp-content/themes/Project_C_2014/header.php
@@ -7,9 +7,20 @@
 <head>
 
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <link rel="author" href="humans.txt">
+
+    <!-- BEGIN WEB FONTS -->
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <!-- END WEB FONTS -->
+
 	<?php wp_head(); ?>
-	
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -17,9 +28,7 @@
 <div>
 
 	<header>
-		
-		<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-			
+
 		<nav>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav>
---
+++ wp-content/themes/Project_C_2014/scripts/organization.js
@@ -0,0 +1,94 @@
+$( document ).ready(function() {
+
+    //at 500px scrolling, change leftColumn to fixed
+    var $leftColumn = $('div.leftColumn');
+
+    var fromTop = function () {
+        var mastHeight = $('.mast').height();
+        if($('body').scrollTop() >= mastHeight + 70){
+            $leftColumn.css({
+                position: 'fixed',
+                top: '30px'
+            });
+        } else{
+            $leftColumn.css({
+                position: 'absolute',
+                top: '0px'
+            });
+        }
+    };
+
+    var playVideo = function(){
+        var iframe = $(".vimeo-iframe")[0];
+        console.log("iframe: " + iframe);
+        var player = $f(iframe);
+        player.api("play");
+    };
+    var pauseVideo = function(){
+        var iframe = $(".vimeo-iframe")[0];
+        console.log("iframe: " + iframe);
+        var player = $f(iframe);
+        player.api("pause");
+    };
+
+
+    fromTop(); //initial setting for leftColumn (used if page loads more than 500px from top already)
+
+    $(document).scroll(function() {
+        //constantly checks for distance from top in order to set leftColumn
+        fromTop();
+    });
+
+
+    $(".playBtn").click(function() {
+        $('.default').fadeOut('400');
+        var windowWidth = $(window).width();
+        if(windowWidth > 1000){
+            newHeight = 563;
+        }
+        else{
+            var newHeight= 0.563 * windowWidth;
+        }
+
+        $('.mast').animate({
+            height: newHeight},
+            400, function() {
+            $('.videoMast').fadeIn(400, function() {
+               setTimeout(function() {
+                     playVideo();
+                     $('.mast').css({
+                         height: 'auto',
+                         overflow: 'visible'
+                     });;
+
+               }, 1000);
+            });
+        });
+
+
+    });
+
+    $(".close").click(function() {
+        pauseVideo();
+        $('.videoMast').fadeOut('400');
+        var windowWidth = $(window).width();
+
+
+        $('.mast').animate({ height: '430px'}, 400, function() {
+                $('.mast').css({
+                    height: '430px',
+                    overflow: 'hidden'
+                });
+                setTimeout(function(){
+                    $('.default').fadeIn(400);
+                });
+
+        });
+    });
+
+$(".iframeContainer").fitVids();
+
+
+
+
+});
